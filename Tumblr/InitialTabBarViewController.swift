23402f7d38cbaed8e6be707d512911592850e10c
JonnYASS <abowdj@gmail.com>
2016-10-16 23:01:24 -0500

    Initial Commit

---
+++ Tumblr/InitialTabBarViewController.swift
@@ -0,0 +1,86 @@
+//
+//  InitialTabBarViewController.swift
+//  Tumblr
+//
+//  Created by Abowd, Jonathan on 10/16/16.
+//  Copyright © 2016 Abowd, Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class InitialTabBarViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    
+    var viewControllers: [UIViewController]!
+    
+    var selectedIndex: Int = 0
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+        searchViewController = storyboard.instantiateViewController(withIdentifier: "SearchViewController")
+        accountViewController = storyboard.instantiateViewController(withIdentifier: "AccountViewController")
+        trendingViewController = storyboard.instantiateViewController(withIdentifier: "TrendingViewController")
+        viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
+        
+        buttons[selectedIndex].isSelected = true
+        didTapButton(buttons[selectedIndex])
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func didTapButton(_ sender: UIButton) {
+        let previousIndex = selectedIndex
+        selectedIndex = sender.tag
+        buttons[previousIndex].isSelected = false
+        
+        let previousVC = viewControllers[previousIndex]
+        previousVC.willMove(toParentViewController: nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        sender.isSelected = true
+        
+        let vc = viewControllers[selectedIndex]
+        addChildViewController(vc)
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        vc.didMove(toParentViewController: self)
+        
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
